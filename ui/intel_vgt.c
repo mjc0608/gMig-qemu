a8478a5d9f284b01d8454983d0de4b0e2ff16dd5
Xiaoguang Chen <xiaoguang.chen@intel.com>
2016-01-18 13:51:00 +0800

    copy vgtbuffer related codes to intel_vgt.c to avoid dependancy to kernel
    Signed-off-by: Xiaoguang Chen <xiaoguang.chen@intel.com>

--- ui/intel_vgt.c
+++ ui/intel_vgt.c
@@ -16,7 +16,6 @@
 
 #define __user
 #include <xf86drm.h>
-#include <drm/i915_drm.h>
 #include <drm_fourcc.h>
 
 #include <SDL.h>
@@ -75,6 +74,45 @@ static EGLContext ctx;
 PFNGLEGLIMAGETARGETTEXTURE2DOESPROC glEGLImageTargetTexture2DOES;
 PFNEGLCREATEIMAGEKHRPROC eglCreateImageKHR;
 PFNEGLDESTROYIMAGEKHRPROC eglDestroyImageKHR;
+
+struct drm_i915_gem_vgtbuffer {
+        __u32 vmid;
+        __u32 plane_id;
+#define I915_VGT_PLANE_PRIMARY 1
+#define I915_VGT_PLANE_SPRITE 2
+#define I915_VGT_PLANE_CURSOR 3
+        __u32 pipe_id;
+        __u32 phys_pipe_id;
+        __u8  enabled;
+        __u8  tiled;
+        __u32 bpp;
+        __u32 hw_format;
+        __u32 drm_format;
+        __u32 start;
+        __u32 x_pos;
+        __u32 y_pos;
+        __u32 x_offset;
+        __u32 y_offset;
+        __u32 size;
+        __u32 width;
+        __u32 height;
+        __u32 stride;
+        __u64 user_ptr;
+        __u32 user_size;
+        __u32 flags;
+#define I915_VGTBUFFER_READ_ONLY (1<<0)
+#define I915_VGTBUFFER_QUERY_ONLY (1<<1)
+#define I915_VGTBUFFER_UNSYNCHRONIZED 0x80000000
+        /**
+         * Returned handle for the object.
+         *
+         * Object handles are nonzero.
+         */
+        __u32 handle;
+};
+
+#define DRM_I915_GEM_VGTBUFFER          0x36
+#define DRM_IOCTL_I915_GEM_VGTBUFFER    DRM_IOWR(DRM_COMMAND_BASE + DRM_I915_GEM_VGTBUFFER, struct drm_i915_gem_vgtbuffer)
 
 static void drawRect(void)
 {
